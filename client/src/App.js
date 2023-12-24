1b5acd173aa6608286f0a23f1a389e1587427062
Pedro Midueño <pedro.midueno@softoil.com.mx>
2023-12-24 17:10:06 -0600

    Redeploy: change backend and db deploy url

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ import Footer from './Components/Footer/Footer';
 
 import axios from 'axios';
 // axios.defaults.baseURL = 'http://localhost:3001/'
-axios.defaults.baseURL = 'https://pi-countries-back-fgm4.onrender.com/'
+axios.defaults.baseURL = 'https://pi-countries-dev-mfrm.3.us-1.fl0.io/'
 
 function App() {
 
